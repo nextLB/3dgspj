8f52e946940f08f09e2267654dbfc745562959c2
next <364574103@qq.com>
2025-12-18 18:54:39 +0800

    save jsons

--- algorithms_and_models/V1_0/dataset.py
+++ algorithms_and_models/V1_0/dataset.py
@@ -2,9 +2,9 @@
     基于Mip_NeRF360无人机影像三维重构数据集构建的程序文件
 """
 
+import json
 import os
 import pycolmap
-from skimage.morphology import reconstruction
 from torch.utils.data import Dataset
 import numpy as np
 
@@ -13,6 +13,8 @@ DIR_SELECT = '360_v2/bicycle'
 RESOLUTION_IMAGE = 'images_8'   # 可以是 images、images_2、images_4、images_8
 SPARSE_PATH = 'sparse/0'
 POSES_NAME = 'poses_bounds.npy'
+SAVE_INTEGRATED_DATA_JSON_PATH = 'integrated_data.json'
+SAVE_3D_POINTS_DATA_JSON_PATH = '3d_points.json'
 
 
 
@@ -117,7 +119,6 @@ class MipNeRF360Dataset(Dataset):
         self.integrateDataInfo.sort(key=lambda x: x['image_id'])
 
         for point_id, point3d in self.points3D.items():
-            # print(point_id, point3d)
             tempPoint3DData = {}
             tempPoint3DData["3d_point_id"] = point_id
             tempPoint3DData["3d_position"] = point3d.xyz
@@ -125,14 +126,12 @@ class MipNeRF360Dataset(Dataset):
 
             self.points3DInfo.append(tempPoint3DData)
 
-        print(self.integrateDataInfo)
-        print(self.points3DInfo)
-
 
     def load_all_data(self):
         self.load_sparse_info()
         self.load_poses_info()
         self.integrate_to_3dgsjs_information()
+        # self.save_integrated_data_to_json()
 
     def matrix_to_quaternion(self, R):
         """将旋转矩阵转换为四元数"""
@@ -172,6 +171,110 @@ class MipNeRF360Dataset(Dataset):
 
     def __getitem__(self, item):
         pass
+
+
+    # 保存整合后的数据到.json文件中
+    def save_integrated_data_to_json(self):
+
+        # 创建可序列化的数据结构
+        serializable_data = []
+
+        # 遍历每个图像的数据
+        for image_data in self.integrateDataInfo:
+            serializable_image_data = {}
+
+            # 复制所有简单的键值对
+            for key, value in image_data.items():
+                if key == 'triangulated' and value is not None:
+                    # 处理Point2D对象列表
+                    serializable_image_data[key] = []
+                    for point2d in value:
+                        # 假设Point2D对象有xy和point3D_id属性
+                        if hasattr(point2d, 'xy') and hasattr(point2d, 'point3D_id'):
+                            serializable_image_data[key].append({
+                                "xy": point2d.xy.tolist() if isinstance(point2d.xy, np.ndarray) else list(point2d.xy),
+                                "point3D_id": int(point2d.point3D_id)
+                            })
+                        else:
+                            # 如果Point2D对象不是预期的类型，尝试直接转换
+                            serializable_image_data[key].append({
+                                "xy": list(point2d.xy) if hasattr(point2d, 'xy') else [],
+                                "point3D_id": int(point2d.point3D_id) if hasattr(point2d, 'point3D_id') else -1
+                            })
+                elif key == 'c2w' and isinstance(value, np.ndarray):
+                    # 将numpy数组转换为列表
+                    serializable_image_data[key] = value.flatten().tolist()
+                elif isinstance(value, np.ndarray):
+                    # 其他numpy数组转换为列表
+                    serializable_image_data[key] = value.tolist()
+                else:
+                    # 其他类型直接复制
+                    serializable_image_data[key] = value
+
+            serializable_data.append(serializable_image_data)
+
+        filename = os.path.join(MIP_NERF360_ROOT, SAVE_INTEGRATED_DATA_JSON_PATH)
+        # 将数据写入JSON文件
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(serializable_data, f, indent=2, ensure_ascii=False)
+
+        # 准备可序列化的数据
+        serializable_data = []
+
+        for point3d_data in self.points3DInfo:
+            serializable_point = {}
+
+            # 复制所有字段
+            for key, value in point3d_data.items():
+                if key == '3d_position' and isinstance(value, np.ndarray):
+                    # 处理3D位置数组
+                    serializable_point[key] = value.tolist()
+
+                elif key == '3d_track_len':
+                    # 处理Track对象
+                    if hasattr(value, 'elements'):
+                        track_elements = []
+                        for element in value.elements:
+                            # 假设TrackElement有image_id和point2D_idx属性
+                            track_element_dict = {}
+
+                            if hasattr(element, 'image_id'):
+                                track_element_dict['image_id'] = int(element.image_id)
+
+                            if hasattr(element, 'point2D_idx'):
+                                track_element_dict['point2D_idx'] = int(element.point2D_idx)
+
+                            track_elements.append(track_element_dict)
+
+                        serializable_point[key] = {
+                            'elements': track_elements,
+                            'track_length': len(track_elements)  # 添加track长度
+                        }
+                    else:
+                        # 如果Track对象没有elements属性，尝试其他方式
+                        serializable_point[key] = str(value)  # 或者尝试其他序列化方式
+
+                else:
+                    # 其他字段直接复制
+                    serializable_point[key] = value
+
+            serializable_data.append(serializable_point)
+
+        filename = os.path.join(MIP_NERF360_ROOT, SAVE_3D_POINTS_DATA_JSON_PATH)
+        # 保存到JSON文件
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(serializable_data, f, indent=2, ensure_ascii=False)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
