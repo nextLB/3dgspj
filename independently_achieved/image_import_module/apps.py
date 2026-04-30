af78c3cee02bdeaaa0bd53e5c92cdb14fdc9ebcc
next <364574103@qq.com>
2026-04-30 09:17:51 +0800

    create

--- independently_achieved/image_import_module/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class ImageImportModuleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'  # 修正路径
-    name = 'image_import_module'
-    verbose_name = '图像导入模块'
